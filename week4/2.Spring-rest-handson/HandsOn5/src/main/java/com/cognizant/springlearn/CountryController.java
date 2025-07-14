f5d0040b85f991c3da6786beeabeb3e0e121cb9f
Akhila-patha <akhilapatha23@gmail.com>
2025-07-14 23:15:27 +0530

    Add files via upload

---
+++ week4/2.Spring-rest-handson/HandsOn5/src/main/java/com/cognizant/springlearn/CountryController.java
@@ -0,0 +1,30 @@
+package com.cognizant.springlearn;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/countries/{code}")
+    public Country getCountryByCode(@PathVariable String code) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        List<Country> list = (List<Country>) context.getBean("countryList");
+
+        return list.stream()
+                   .filter(c -> c.getCode().equalsIgnoreCase(code))
+                   .findFirst()
+                   .orElseThrow(() -> new CountryNotFoundException("Country not found: " + code));
+    }
+
+    @ExceptionHandler(CountryNotFoundException.class)
+    public String handleCountryNotFound(CountryNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
