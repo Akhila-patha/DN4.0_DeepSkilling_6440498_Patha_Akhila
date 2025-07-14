0383b44edf857fd56092d4dba5b16c5765189c99
Akhila-patha <akhilapatha23@gmail.com>
2025-07-14 23:18:07 +0530

    Add files via upload

---
+++ week4/2.Spring-rest-handson/HandsOn6/src/main/java/com/cognizant/springlearn/CountryController.java
@@ -0,0 +1,18 @@
+package com.cognizant.springlearn;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        return (List<Country>) context.getBean("countryList");
+    }
+}
